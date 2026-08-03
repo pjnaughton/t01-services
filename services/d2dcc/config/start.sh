8a220e3e407e7d4a0ade3acb968c0fd0a5d65bbf
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-03 17:38:11 +0100

    Use sourcewhenstarting scripts

--- services/d2dcc/config/start.sh
+++ services/d2dcc/config/start.sh
@@ -1,9 +1,9 @@
 if [ "$FPGA_NAME" = "DPS" ]; then
     echo "Running DPS Start Script"
-    ${IOC}/config/dps_start.sh
+    source ${IOC}/config/dps_start.sh
 elif [ "$FPGA_NAME" = "FRC" ]; then
     echo "Running FRC Start Script"
-    ${IOC}/config/frc_start.sh
+    source ${IOC}/config/frc_start.sh
 else
     echo "Invalid FPGA Name $FPGA_NAME"
     exit 1
