edcfdc06eb2066acb8635ce6f1bf72cc7f923278
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-03 17:32:59 +0100

    Make start up script for different soft iocs

--- services/d2dcc/config/start.sh
+++ services/d2dcc/config/start.sh
@@ -1,1 +1,10 @@
-/epics/support/d2dcc/bin/linux-x86_64/sim_tcp_server $OPTIONS $FPGA_NAME $FPGA_NUMBER
+if [ "$FPGA_NAME" = "DPS" ]; then
+    echo "Running DPS Start Script"
+    ${IOC}/config/dps_start.sh
+elif [ "$FPGA_NAME" = "FRC" ]; then
+    echo "Running FRC Start Script"
+    ${IOC}/config/frc_start.sh
+else
+    echo "Invalid FPGA Name $FPGA_NAME"
+    exit 1
+fi
