138982fe2ea548b4375dbfa9370b96cf3024d4b7
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-07 10:20:42 +0100

    Move init script from d2dcc-ioc

--- services/t01-d2dcc/config/start.sh
+++ services/t01-d2dcc/config/start.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 set -e
 
-cd $D2DCC_DIR
 #####  Start the server #####
 
-TOOLS_PATH=tcpServerApp/tools
+sed -i "s/@@FPGA_TYPE@@/$FPGA_TYPE/" ${CONFIG_DIR}/env
+sed -i "s/@@FPGA_TYPE_NUM@@/$FPGA_TYPE_NUM/" ${CONFIG_DIR}/env
 
-sed -i "s/@@FPGA_TYPE@@/$FPGA_TYPE/" $TOOLS_PATH/env
-sed -i "s/@@FPGA_TYPE_NUM@@/$FPGA_TYPE_NUM/" $TOOLS_PATH/env
-
-expect $TOOLS_PATH/init_server.exp
+expect ${CONFIG_DIR}/init_server.exp
 
 ##### Start Soft IOC #####
 if [[ "$FPGA_TYPE" == *"DPS"* ]]; then
     echo "Running DPS Start Script"
-    IOC_DIR=dpsApp
+    IOC_DIR=${D2DCC_DIR}/dpsApp
 elif [ "$FPGA_NAME" = "SOFB" ] || [ "$FPGA_NAME" = "FOFB" ]; then
     echo "Running FRC Start Script"
-    IOC_DIR=frcApp
+    IOC_DIR=${D2DCC_DIR}/frcApp
 else
     echo "Invalid FPGA Name $FPGA_NAME"
     exit 1
