f5bd611736594bb4d880f007e5390bfdddb6dc54
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-10 09:08:25 +0100

    Set enabled var to true

--- ec-setup.sh
+++ ec-setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# load ec configured for the training beamline P47
-module load ec/p47
+# load ec configured for the training beamline user
+module load ec/user
 
 # re-configure it to point at your personal namespace
 EC_SERVICES_REPO=https://github.com/pjnaughton/t01-services
 EC_TARGET=ygn40972/t01
 
 # load argus configuration for kubectl
-module load argus
-
+module load pollux
