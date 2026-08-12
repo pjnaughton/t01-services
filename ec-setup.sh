992f4516c98ed5a5c9d79121df3db5f948fe0772
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-12 15:46:42 +0100

    Fix name of liveness script

--- ec-setup.sh
+++ ec-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # load ec configured for the training beamline user
-module load ec/user
+module load ec/p47
 
 # re-configure it to point at your personal namespace
 EC_SERVICES_REPO=https://github.com/pjnaughton/t01-services
 EC_TARGET=accelerator/t01
 
 # load argus configuration for kubectl
-module load pollux
+module load argus
