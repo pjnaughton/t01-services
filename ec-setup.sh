d582cd2a90ff7a135d182f8cd5f8cea8be5305db
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-12 15:35:58 +0100

    Removing redundant services repo

--- ec-setup.sh
+++ ec-setup.sh
@@ -5,7 +5,7 @@ module load ec/user
 
 # re-configure it to point at your personal namespace
 EC_SERVICES_REPO=https://github.com/pjnaughton/t01-services
-EC_TARGET=ygn40972/t01
+EC_TARGET=accelerator/t01
 
 # load argus configuration for kubectl
 module load pollux
