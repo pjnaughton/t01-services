ae740092bb5fb47a79628c14d0801c0d8fcf150f
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-18 15:19:55 +0100

    Added config to ts0001 and added notes to script

---
+++ services/.d2dcc_config/liveness.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TOP=/epics/ioc
+cd ${TOP}
+CONFIG_DIR=${TOP}/config
+
+set -ex
+
+case "$FPGA_TYPE" in
+    SIC_DPS)
+        LIVENESS_PV=( "${NAME}:STATE")
+        ;;
+    SOFB | FOFB)
+        for MAGNET_NAME in "MAGNET_ONE" "MAGNET_TWO" "MAGNET_THREE" "MAGNET_FOUR"; do
+            if [[ ! -z "${MAGNET_NAME}" ]]; then
+                LIVENESS_PV+=( "${MAGNET_NAME}:STATE")
+            fi
+        done
+        ;;
+    *)
+        echo "Invalid FPGA Name $FPGA_TYPE"
+        exit 1
+        ;;
+esac
+
+for pv in ${LIVENESS_PV[@]}; do
+    if ! caget ${pv} ; then
+        echo "Liveness check failed for ${IOC_NAME}" > /proc/1/fd/1
+        echo "Failing PV: ${pv}" > /proc/1/fd/2
+        exit 1
