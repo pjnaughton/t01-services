9ea35f095597ac4cd62a52b04bc469a4b5e59c61
Naughton, Paul (DLSLtd,RAL,DOD) <paul.naughton@diamond.ac.uk>
2026-08-24 13:15:12 +0100

    FIx liveness probe

--- services/.d2dcc_config/liveness.sh
+++ services/.d2dcc_config/liveness.sh
@@ -11,8 +11,8 @@ case "$FPGA_TYPE" in
         ;;
     SOFB | FOFB)
         for MAGNET_NAME in "MAGNET_ONE" "MAGNET_TWO" "MAGNET_THREE" "MAGNET_FOUR"; do
-            if [[ ! -z "${MAGNET_NAME}" ]]; then
-                LIVENESS_PV+=( "${MAGNET_NAME}:STATE")
+            if [[ ! -z "${!MAGNET_NAME}" ]]; then
+                LIVENESS_PV+=( "${!MAGNET_NAME}:STATE")
             fi
         done
         ;;
